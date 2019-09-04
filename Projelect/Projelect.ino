7f8d43daeaf7e8ebce36cacff9b018e7365e8f5b
world-of-peb <baudin.pierreeudes@yahoo.fr>
2019-09-04 15:33:24 +0200

    V1

--- Projelect/Projelect.ino
+++ Projelect/Projelect.ino
@@ -1,19 +1,56 @@
-#include <LiquidCrystal.h>
-
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialise la broche 10 et 4 en sortie numérique
-  // 
-  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
+void printLed(int nmbr) {
+  for (int i=1; i <= 6; i++){
+      analogWrite(i, 0);
+      delay(10);
+   } 
+   analogWrite(nmbr, 255);
+}
+
+void errorLed(int nmbr) {
+  for (int i=1; i <= 6; i++){
+      analogWrite(i, 255);
+      delay(10);
+   } 
+   analogWrite(nmbr, 0);
+}
+
+
+// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(10, HIGH);
-  digitalWrite(4, LOW);
-  delay(100);
-  digitalWrite(4, HIGH);
-  digitalWrite(10, LOW);
-  delay(100);
-}
+  int captMouv  = analogRead(0);
+  Serial.println(captMouv);
+  int val = 0;
 
+  if (captMouv < 200) {
+    printLed(2);
+    }
+
+  if (captMouv > 200 && captMouv < 300) {
+    printLed(3);
+  }
+ 
+  if (captMouv > 300 && CaptMouv <400) {
+    printLed(4);
+  }
+    
+  if (captMouv > 400 && captMouv < 500) {
+    printLed(5); 
+  }
+  if (captMouv > 500 && captMouv < 600){
+    printLed(6);
+  }
+  
+    
+  //Serial.println(l);
+  delay(1000);
+}
